3f0f7c7beee64a7fd21158debf8b59a7d376ec67
charlielovesdata <122847674+cbeierlbuffs@users.noreply.github.com>
2023-02-25 16:37:14 -0600

    Preliminary work and test on FakeRepo

--- Barky/Models/bookmark.py
+++ Barky/Models/bookmark.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 @dataclass
 class BookMark:
@@ -7,6 +8,9 @@ class BookMark:
     url: str
     notes: str
     date_added: str
+
+    def __lt__(self, other):
+        return datetime.strptime(self.date_added, '%m/%d/%y') < datetime.strptime(other.date_added, '%m/%d/%y')
 
 
 #newbookmark = BookMark(1,"TestTitle","hxxp://www.google.com","Test note check","02/24/2023")
--- Barky/Repository/repositories.py
+++ Barky/Repository/repositories.py
@@ -1,12 +1,43 @@
 from repository.baserepo import AbstractRepository
+from models.bookmark import BookMark
 
 class FakeRepository(AbstractRepository):
     def __init__(self):
-        self.seen = set()
+        self.seen = []
 
+    def BookmarkAdd(self, bookmark: BookMark): 
+        self.bookmark = bookmark
+        self.seen.append(bookmark)
 
+    def BookmarkDelete(self, id: int): 
+        self.deleteindex = id
+        for bm in self.seen:
+            if bm.id == id:
+                self.seen.remove(bm)
+    
+    def BookMarkList_by_Date(self):
+        self.seen.sort()
+    
+    def BookMarkList_by_Title(self):
+        self.seen.sort(key=lambda x: x.title)
+
+#Run a little test    
+newBm1 = BookMark(3,"ATest1FakeRepo","hxxp://www.fakerepo.com/test1","kitty fiddle","02/27/23")
+newBm2 = BookMark(4,"FTest10FakeRepo","hxxp://www.fakerepo.com/test2","kitty fiddle","02/25/23")
+newBm3 = BookMark(10,"CTest2FakeRepo","hxxp://www.fakerepo.com/test3","kitty fiddle","02/25/20")
 Fr = FakeRepository()
+Fr.BookmarkAdd(newBm1)
+Fr.BookmarkAdd(newBm2)
+Fr.BookmarkAdd(newBm3)
+print("ORIGINAL\n",Fr.seen,"\n")
+Fr.BookMarkList_by_Date()
+print("SORT BY DATE\n",Fr.seen,"\n")
+Fr.BookMarkList_by_Title()
+print("SORT BY TITLE\n",Fr.seen,"\n")
+Fr.BookmarkDelete(4)
+print("POST DELETE:\n",Fr.seen)
+
 
 '''
-Run module individually like this: python -m repository.repositories
+Run module individually like this: <topprojectdir>\python -m repository.repositories
 '''
